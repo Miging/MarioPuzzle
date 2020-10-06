badb2d27a0e57e051dd9d9e13ed9910b5c44f7e0
dnfldpden <dnfldpden32@gmail.com>
2020-10-06 12:27:53 +0900

    Modified MArioPuzzle.py[MOD}

--- MarioPuzzle.py
+++ MarioPuzzle.py
@@ -1,61 +1,81 @@
 from bangtal import *
+import random
 
-scene=Scene("","images/problem.png")
+setGameOption(GameOption.INVENTORY_BUTTON,False)
+setGameOption(GameOption.MESSAGE_BOX_BUTTON,False)
 
-ob1=Object("images/1.png")
-ob2=Object("images/2.png")
-ob3=Object("images/3.png")
-ob4=Object("images/4.png")
-ob5=Object("images/5.png")
-ob6=Object("images/6.png")
-ob7=Object("images/7.png")
-ob8=Object("images/8.png")
-ob9=Object("images/9.png")
-ob10=Object("images/10.png")
-ob11=Object("images/11.png")
-ob12=Object("images/12.png")
-ob13=Object("images/13.png")
-ob14=Object("images/14.png")
-ob15=Object("images/15.png")
-ob16=Object("images/16.png")
+scene=Scene("마리오","images/problem.png")
 
-ob1.locate(scene,360,500)
-ob2.locate(scene,500,500)
-ob3.locate(scene,640,500)
-ob4.locate(scene,780,500)
-ob5.locate(scene,360,360)
-ob6.locate(scene,500,360)
-ob7.locate(scene,640,360)
-ob8.locate(scene,780,360)
-ob9.locate(scene,360,220)
-ob10.locate(scene,500,220)
-ob11.locate(scene,640,220)
-ob12.locate(scene,780,220)
-ob13.locate(scene,360,80)
-ob14.locate(scene,500,80)
-ob15.locate(scene,640,80)
-ob16.locate(scene,780,80)
+def find_index(object):
+    for index in range(16):
+        if game_board[index]==object:return index
 
-ob1.show()
-ob2.show()
-ob3.show()
-ob4.show()
-ob5.show()
-ob6.show()
-ob7.show()
-ob8.show()
-ob9.show()
-ob10.show()
-ob11.show()
-ob12.show()
-ob13.show()
-ob14.show()
-ob15.show()
+def movable(index):
+    if index<0:return False
+    if index>16:return False
+    if index%4>0and index-1==blank:return True
+    if index%4<3and index+1==blank:return True
+    if index>3 and index-4==blank:return True
+    if index<12 and index+4==blank:return True
+    return False
 
-list1=[[ob1,ob2,ob3,ob4],[ob5,ob6,ob7,ob8],[ob9,ob10,ob11,ob12],[ob13,ob14,ob15,None]]
+def move(index):
+    global blank
 
-def isempty(ob):
+    game_board[index].locate(scene,360+140*(blank%4),500-140*(blank//4))
+    game_board[blank].locate(scene,360+140*(index%4),500-140*(index//4))
+
+    game_board[index],game_board[blank]= game_board[blank],game_board[index]
+    #object=game_board[index]
+    #game_board[index]=game_board[blank]
+    #game_board[blank]=object
+
+    blank=index
+def completed():
+    for index in range(16):
+        if game_board[index]!=init_board[index]:return False
+    return True
+
+def onMouseAction_piece(object,x,y,action):
+    index=find_index(object)
+    if movable(index):
+        move(index)
+        if completed()==True:showMessage("끗")
+Object.onMouseActionDefault=onMouseAction_piece
+
+game_board=[]
+init_board=[]
+for index in range(16):
+    piece=Object("images/"+str(index+1)+".png")
+    piece.locate(scene,360+140*(index%4),500-140*(index//4))
+    piece.show()
+
+    game_board.append(piece)
+    init_board.append(piece)
+
+blank=15
+game_board[blank].hide()
+
+delta=[-1,1,-4,4]
+def random_move():
+    while True:
+        index=blank+delta[random.randrange(4)]
+        if movable(index): break
+    move(index)
 
 
+count =50
+timer =Timer(1)
+def onTimeout():
+    random_move()
+    global count 
+    print(count)
+    count =count-1
+    if count>0:
 
+        timer.set(0.1)
+        timer.start()
+timer.onTimeout=onTimeout
+
+timer.start()
 startGame(scene)
